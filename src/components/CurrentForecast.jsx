ebd857fe2a71a211196d544de83265a50587fb1f
GuzmanAriel <guzmanariel525@gmail.com>
2023-07-14 16:53:38 -0500

    Set up data for current forecast

--- src/components/CurrentForecast.jsx
+++ src/components/CurrentForecast.jsx
@@ -1,16 +1,52 @@
 import thunderstorms from "../assets/icons/thunderstorms.svg";
+import { useEffect, useState
+ } from "react";
 function CurrentForecast () {
+    const [locationName, setLocationName] = useState("");
+    const [temp, setTemp] = useState(null);
+    const [description, setDescription] = useState("");
+    //setCurrentDate(); 
+
+    const returnCurrentDate = () => {
+        const today = new Date();
+        const month = today.getMonth();
+        const date = today.getDate();
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+        return `${monthNames[month]} ${date}`;
+    }
+
+    const returnTemp = () => {
+        const temperature = Math.trunc(temp);
+        
+
+        return temperature;
+    }
+    
+    useEffect(() => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=30.2672&lon=-97.733330&exclude=hourly,daily,minutely,alerts&units=imperial&appid=5a469e083d65e3be67d4d3cab2da6ec9')
+          .then(response => response.json())
+          .then(data => {
+            setLocationName(data.name);
+            setTemp(data.main.temp);
+            setDescription(data.weather[0].description);
+            console.log('%cCurrentForecast.jsx line:12 data', 'color: #007acc;', data.weather[0].description);
+          })
+          .catch(error => console.error(error));
+      }, []);
+
+
     return(
         <div className="wa__view wa__view-current">
             <div className="wa__view-current-top">
-                <h1 className="wa__view-location">Austin</h1>
-                <p className="wa__view-date">Jan 18</p>
+                <h1 className="wa__view-location">{locationName}</h1>
+                <p className="wa__view-date">{returnCurrentDate()}</p>
             </div>  
             <div className="wa__view-current-bottom">
                 <img className="wa__view-icon" src={thunderstorms} alt="Thunderstorms Icon"/>
                 <div className="wa__view-weather">
-                    <p className="wa__view-weather-temp">72°</p>
-                    <p className="wa__view-weather-desc">Thunderstorms</p>
+                    <p className="wa__view-weather-temp">{returnTemp()}°</p>
+                    <p className="wa__view-weather-desc">{description}</p>
                 </div>
             </div>       
         </div>
--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -3,31 +3,31 @@ function DailyForecast () {
     return (
         <div className="wa__view wa__view-daily">
             <div className="wa__view-daily-container">
-                <div className="wa__view-daily">
+                <div className="wa__view-day">
                     <p className="wa__view-daily-low">L: 24°</p>
                     <p className="wa__view-daily-high">H: 49°</p>
                     <img className="wa__view-hour-icon" src={thunderstorms} alt="Thunderstorms"/>
                     <p className="wa__view-daily-day">Mon</p>
                 </div>
-                <div className="wa__view-daily">
+                <div className="wa__view-day">
                     <p className="wa__view-daily-low">L: 24°</p>
                     <p className="wa__view-daily-high">H: 49°</p>
                     <img className="wa__view-hour-icon" src={thunderstorms} alt="Thunderstorms"/>
                     <p className="wa__view-daily-day">Tues</p>
                 </div>
-                <div className="wa__view-daily">
+                <div className="wa__view-day">
                     <p className="wa__view-daily-low">L: 24°</p>
                     <p className="wa__view-daily-high">H: 49°</p>
                     <img className="wa__view-hour-icon" src={thunderstorms} alt="Thunderstorms"/>
                     <p className="wa__view-daily-day">Wed</p>
                 </div>
-                <div className="wa__view-daily">
+                <div className="wa__view-day">
                     <p className="wa__view-daily-low">L: 24°</p>
                     <p className="wa__view-daily-high">H: 49°</p>
                     <img className="wa__view-hour-icon" src={thunderstorms} alt="Thunderstorms"/>
                     <p className="wa__view-daily-day">Thurs</p>
                 </div>
-                <div className="wa__view-daily">
+                <div className="wa__view-day">
                     <p className="wa__view-daily-low">L: 24°</p>
                     <p className="wa__view-daily-high">H: 49°</p>
                     <img className="wa__view-hour-icon" src={thunderstorms} alt="Thunderstorms"/>
